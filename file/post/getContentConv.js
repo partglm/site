9fa812063eddac280b7191176d7818e0a22cbe4e
Pierre <143663492+partglm@users.noreply.github.com>
2025-05-01 19:48:05 +0000

    i'm in love with v0

--- file/post/getContentConv.js
+++ file/post/getContentConv.js
@@ -1,12 +1,13 @@
 const conv = require('../conv')
 const app = require('../app')
+const { log } = require('../logger')
 
 const routergetContentConv = app.express.Router()
 
-routergetContentConv.post('/', (req,res) => {
+routergetContentConv.post('/', async (req,res) => {
     const convData = new conv("a", "a")
-    const contentConv = convData.getconversationContentWithNameOfIt(req.body.param)
-
+    const contentConv = await convData.getconversationContentWithNameOfIt(req.body.param)
+    new log("the content for " + req.body.param + " have loaded succesfully: ")
     res.json({content: contentConv})
 })
 
