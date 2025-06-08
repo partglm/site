c9add6c83ecfe8ef10bc879b6970f3a4cf94f919
Pierre <143663492+partglm@users.noreply.github.com>
2025-06-08 18:20:18 +0000

    error on req.status at linux.js at  verify acess

--- file/post/linux.js
+++ file/post/linux.js
@@ -4,14 +4,17 @@ const ADMIN = require('../admin');
 const { ADMIN_PANNEL } = require('../config');
 const routerLinux = app.express.Router()
 
-new log("post method auth loaded success")
+
 routerLinux.post('/', async(req,res) => {
     const {cmd} = req.body
-    if (!ADMIN_PANNEL || ADMIN.oneSessionIDauth(req.cookies.oneSessionID) !== true) return req.status(403).json({err: "you can't access to this"})
+    
+    if (!ADMIN_PANNEL || ADMIN.oneSessionIDauth(req.cookies.oneSessionID) !== true) return res.status(403).json({err: "you can't access to this"})
 
     if (!app.terminal._ready) {
         res.status(401).json({err: 'please wait a few second'})
     }
+    new log("acces granted to the terminal")
+
     const result = await app.terminal.runCommand(cmd)
     if(!result) {
         return res.status(200).json({result: true})
